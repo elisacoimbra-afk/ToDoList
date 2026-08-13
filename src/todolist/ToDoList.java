fa245ce4c61ace4088d07c5ff9e2ed8b94d2eea7
elisa <elisa.coimbra@aluno.senai.br>
2026-08-13 10:56:19 -0300

    Altersções aula 13/08

--- src/todolist/ToDoList.java
+++ src/todolist/ToDoList.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package todolist;
 
 /**
@@ -14,7 +11,23 @@ public class ToDoList {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sacanner = new Scanner (System.in);
+        String[] tarefas = new string [10];
+        boolean[] concluidas = new boolean[10];
+        
+        int opcao = 0;
+        
+        
+        while (opcao !=5) {
+            System.out.println("\n === LISTA DE TAREFAS ===");
+            System.out.println("1- ADICIONAR TAREFA");
+            System.out.println("2- LISTAR TAREFA");
+            System.out.println("3- CONCLUIR TAREFA");
+            System.out.println("4- EXCLUIR TAREFA");
+            System.out.println("5- SAIR");
+            
+        }
     }
     
 }
+
